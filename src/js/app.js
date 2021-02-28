14b18ccd3baaed2e110d1d98ccc04416d9e5006d
Konrad <konradzuse.dm@gmail.com>
2021-02-28 14:36:08 -0500

    created a new branch to self test be able to recreaate same fuunctionality with JQuery

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,32 @@
-  $(document).ready(function(){
-     // --- Modify the text Property on the div with the id "content"  when the button bt2 is clicked
-     $('#bt1').click(function(){
-         alert("Hi JQuery, I'm a live!!");
-     });
+  $('document').ready(function(){ // -- This is the JQuery wrapper to the active code
 
+  	 // --- Use a # selector to bind a click event to thee button with id="bt1" to run an Alert
+  	  $('#bt1').click(function(){
+  	  	 alert("Here I'm a live!!");
+  	  });
+  	  
+  	  // --- Use a . selector to caught a node on the DOM by its className
+  	  $('.bt2').click(function(){
+  	  	 $('#content').text("Here is a text addded when the click trigered the button bt2.")
+  	  });
+  	  
+  	  $('#clearDiv').click(function(){
+  	  	  $('#content').text('');
+  	  });
     
-     // --- Modify the text Property on the div with the id "content"  when the button bt2 is clicked
-     $('#bt2').click(function(){
-        $('#content').text('Text added by trigering the click over the bt2.');
-     });
-
-     // --- Modify the text property on the div to clear its content to NULL
-     $('#bt3').click(function(){
-         $('#content').text('');
-     });
-    
-     // -- Create a new counter with twoe buttons add ++ && minus --
-     var c = 0;
-     $('#add').click(function(){
-         $('#count').text(++c);
-     });
-
-     $('#minus').click(function(){
-          $('#count').text(--c);
-     });
-     
-     $('#countAlert').click(function(){
-   
-        alert($('#counting').text());
-     });
-
+  	  var c = 0;
+  	  
+  	  $('#add').click(function(){
+  	  	  $('#count').text(++c);
+  	  });
+  	  
+  	  $('#minus').click(function(){
+  	  	  $('#count').text(--c);
+  	  });
+  	  
+  	  
+  	  $('#countAlert').click(function(){
+  	    	(($('#count').text())=== '0') ?alert( ' Nothing to be showed '): alert("CounterStats:  "+$('#count').text());
+  	  	
+  	  });
   });
-  // --- To do the same with ES6/ES7/ even ES9 ??
-
